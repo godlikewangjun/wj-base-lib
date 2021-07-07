489f172f6d7995e7c1160ac4cd20aba8da38213f
wangjun <123456>
2021-07-07 09:25:08 +0800

    提示默认在UI线程

--- ktlibrary/src/main/java/com/wj/ktutils/StringUilts.kt
+++ ktlibrary/src/main/java/com/wj/ktutils/StringUilts.kt
@@ -3,6 +3,7 @@
 @file:kotlin.jvm.JvmName("WjStringsKt")
 package com.wj.ktutils
 
+import android.app.Activity
 import android.content.Context
 import com.abase.util.AbStrUtil
 import com.abase.util.ToastUtil
@@ -23,7 +24,10 @@ inline fun CharSequence?.isEmail(): Boolean =
         AbStrUtil.isEmail(this.toString())
 
 inline fun Context?.showTip(string: String) =
-        this?.runOnUiThread { ToastUtil.showTip(this,string) }
+        this!!.runOnUiThread { ToastUtil.showTip(this,string) }
 
 inline fun androidx.fragment.app.Fragment?.showTip(string: String) =
-        this?.runOnUiThread { ToastUtil.showTip(this.context,string) }
+        this!!.runOnUiThread { ToastUtil.showTip(this.context,string) }
+
+inline fun Activity?.showTip(string: String) =
+        this!!.runOnUiThread { ToastUtil.showTip(this,string) }
